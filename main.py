158e5679828b961665bf08c11ae77a19befcc962
suarjana16 <wayansuarjana602@gmail.com>
2025-06-18 23:38:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram.ext import Updater, CommandHandler
 from datetime import datetime, time as dtime
 import pytz
 
-TOKEN = os.getenv("7875159098:AAHA17VazBaHRLrZ3zaO_Yjic0tD6rAnMVI")
+TOKEN = os.getenv("BOT_TOKEN")
 ALERT_TIMES = [
     os.getenv("ALERT_TIME_1", "09:00"),
     os.getenv("ALERT_TIME_2", "18:00"),
@@ -37,8 +37,6 @@ def send_price_alert(context):
 def start(update, context):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=chat_id, text="Bot aktif. Kamu akan menerima notifikasi harga Cardano (ADA) setiap hari.")
-
-    # Jadwalkan notifikasi harian
     tz = pytz.timezone("Asia/Makassar")
     for alert_time in ALERT_TIMES:
         hour, minute = map(int, alert_time.split(":"))
